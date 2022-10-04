bdbd7b9379e15ac7bdaf404ce486023e58277815
boogiman1 <boowon1@naver.com>
2022-10-04 11:22:46 +0900

    포트폴리오 커밋

--- js/custom.js
+++ js/custom.js
@@ -27,18 +27,44 @@ document.addEventListener('DOMContentLoaded',function(){
         // scrollON(winSct)
     })
 
-    // 이미지 이동
-    const hiddens=document.querySelectorAll('.publ_box')
-    const thumbs=document.querySelectorAll('.publ_box img')
-    hiddens.forEach(function(hidden){
-        hidden.addEventListener("mouseenter",function(e){
-            e.preventDefault();
-            console.log(e);
-            const tg=e.target;
-            const tgH=tg.offsetHeight;
-            const tgThumb=tg.firstChild;
-            
+    // 컨텐츠 스크롤 시 나오게
+    window.addEventListener('load',function(){
+        addEventListener('scroll',function(){
+            if(pageYOffset>=600||scrollY>=600){
+                this.document.querySelector("#section2 .container .section2_textBox").classList.add('on')
+            }
+            if(pageYOffset>=2400||scrollY>=2400){
+                var boxWrap = document.querySelectorAll("#section4 .container .publ_container .publ_box")
+                boxWrap[0].classList.add('on')
+                boxWrap[1].classList.add('on')
+
+            }
+            if(pageYOffset>=2800||scrollY>=2800){
+                var boxWrap = document.querySelectorAll("#section4 .container .publ_container .publ_box")
+                boxWrap[2].classList.add('on')
+                boxWrap[3].classList.add('on')
+            }
+            if(pageYOffset>=3450||scrollY>=3450){
+                this.document.querySelector("#section5 .container .design_container .design_text").classList.add('on')
+                this.document.querySelector("#section5 .container .design_container .design_img").classList.add('on')
+            }
         })
-        console.log(hidden);
     })
+
+    // top_btn
+    // const topBtn=document.querySelector('.top_btn');
+    // const circle0=document.querySelector('.top_btn .circle0');
+    // topBtn.children.forEach((el)=>{
+    //     el.addEventListener('click',function(e){
+    //         e.preventDefault()
+    //         document.querySelector(el.firstChild.getAttribute("href")).scrollIntoView({behavior:"smooth"})
+    //     })
+    //     circle0.addEventListener('click',function(e){
+    //         e.preventDefault();
+    //         if(circle0){
+    //             circle0.classList.add('on');
+    //         }
+    //         else{circle0.classList.remove('on');}
+    //     })
+    // })
 })
